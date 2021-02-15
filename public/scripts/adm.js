507abf02650be2157574e04ad778b37ef454ea17
jpwf <joaopedroweydt@gmail.com>
2021-02-14 22:29:54 -0300

    New functions

--- public/scripts/adm.js
+++ public/scripts/adm.js
@@ -1,20 +1,35 @@
 const login = {
     user: "jpwf",
     pass: "cl912222"
+        }
+
+function authenticated(){
+    setTimeout(() => {
+        window.location="/root"
+    }, 1000);
 }
 
+function invasor(){
+    setTimeout(() =>{
+        window.location="/blocked"
+        }, 1000)
+}
 
 
 
 button = document.getElementById("sendb")
 
 
-button.addEventListener("click" ,function(){
+    
+button.addEventListener("click" ,(req,res) => {
     var usuario = document.getElementById("username").value;
     var senha = document.getElementById("pass").value;
     if(usuario==login.user && senha==login.pass){
-        home()
+        authenticated()
+    }else{
+        invasor()
     }
+    
 })
 
 
