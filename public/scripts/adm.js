e902f42c9493230f8d542d7bfaede4420cf05d37
jpwf <joaopedroweydt@gmail.com>
2021-02-16 14:59:03 -0300

    New function added

--- public/scripts/adm.js
+++ public/scripts/adm.js
@@ -1,18 +1,23 @@
 const login = {
     user: "jpwf",
-    pass: "cl912222"
+    pass: "cl912222",
+    key: 2511
         }
 
 function authenticated(){
     setTimeout(() => {
         window.location="/root"
-    }, 1000);
+    }, 500);
+}
+
+function skey(){
+    
 }
 
 function invasor(){
     setTimeout(() =>{
         window.location="/blocked"
-        }, 1000)
+        }, 500)
 }
 
 
@@ -25,7 +30,15 @@ button.addEventListener("click" ,(req,res) => {
     var usuario = document.getElementById("username").value;
     var senha = document.getElementById("pass").value;
     if(usuario==login.user && senha==login.pass){
-        authenticated()
+        skey = prompt("Chave secreta :")
+        if(skey==login.key){
+            confirm(`Bem vindo "${login.user}"`)
+            authenticated()
+        }else{
+            document.write("Invasor")
+            invasor()
+        }
+        
     }else{
         invasor()
     }
