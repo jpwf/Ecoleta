4d772e8a2e174b3805ca9c374e8a8483d7287b25
jpwf <joaopedroweydt@gmail.com>
2021-04-02 17:03:05 -0300

    button´s functions configured

--- public/scripts/adm.js
+++ public/scripts/adm.js
@@ -79,4 +79,3 @@ button.addEventListener("click" ,function(){
 })
 
 
-
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,13 @@
-const buttonsearch = document.querySelector("#page-home main a")
+const buttonsearch = document.querySelector("#page-home main button")
 const modal = document.querySelector("#modal")
-const close = document.querySelector("#modal .header a")
+const close = document.querySelector("#modal header a")
+const admway = document.getElementById("admb")
+const createpoint = document.getElementById("create-point")
+
+
+
+
+
 buttonsearch.addEventListener("click", () => {
     modal.classList.remove("hide")
 })
@@ -9,4 +16,12 @@ buttonsearch.addEventListener("click", () => {
 close.addEventListener("click", () => {
     modal.classList.add("hide")
 
+})
+
+admway.addEventListener("click", () =>{
+    window.location = "/adm"
+})
+
+createpoint.addEventListener("click", () =>{
+    window.location = "/create-point"
 })
