89bd6799c3dec1a8b6ce6f4977d4066ac73267e6
jpwf <joaopedroweydt@gmail.com>
2021-04-10 19:18:42 -0300

    Configured to run with the new tag form

--- public/scripts/adm.js
+++ public/scripts/adm.js
@@ -14,7 +14,7 @@ const logins = {
         jpwf:4321,
         user2:7890
     }  
-        }
+}
 
 var user1 = logins.users.user1.name
 var pass1 = parseInt(logins.users.user1.passw)
@@ -36,46 +36,48 @@ function skey(){
     
 }
 
-function invasor(){
-    document.write("invasor")
-    setTimeout(() =>{
-        window.location="/blocked"
-        }, 500)
-}
+
+// function invasor(){
+//     document.write("invasor")
+//     setTimeout(() =>{
+//         window.location="/blocked"
+//         }, 500)
+// }
 
 
 
-button = document.getElementById("sendb")
 
 
-    
-button.addEventListener("click" ,function(){
-    var usuario = document.getElementById("username").value;
-    var senha = document.getElementById("pass").value;
-    if(usuario==user1 && senha==pass1 || usuario==user2 && senha==pass2){
-        
-        skey = prompt("Chave secreta :")
-        if(skey==key1){
-            confirm(`Bem vindo "${user1}"`)
-            card1 = true
-            authenticated()
+
+const validate = (event) =>{
+    let enter = event.keyCode;
+    if(enter==13){
+
+        var usuario = document.getElementById("username").value
+        var senha = document.getElementById("pass").value
+        if (usuario == user1 && senha == pass1 || usuario == user2 && senha == pass2) {
+
+            skey = prompt("Chave secreta :")
+            if (skey == key1) {
+                confirm(`Bem vindo "${user1}"`)
+                card1 = true
+                authenticated()
+            }
+
+            else if (skey == key2) {
+                confirm(`Bem vindo "${user2}"`)
+                card2 = true
+                authenticated()
+            }
+
+
         }
-    
-        else if(skey==key2){
-            confirm(`Bem vindo "${user2}"`)
-            card2 = true
-            authenticated()
-        }
-        
-        
-    }
-    
-    if(usuario!=user1 && senha!=pass1 && usuario!=user2 && senha!=pass2 ){
-        alert("Login recusado")
-        
-    }
-    
-    
-})
 
+        if (usuario != user1 && senha != pass1 && usuario != user2 && senha != pass2) {
+            alert("Login recusado")
+            window.location = "/"
+
+        }
+}}
+document.body.onkeypress = validate;
 
